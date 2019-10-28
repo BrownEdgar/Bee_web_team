7b3381b1425bc4560cb2198650d0fc1ee557c3ef
Edgar Nikoghosyan <nikogosjan_edgar@mail.ru>
2019-10-28 17:57:19 +0400

    add two new folder-views/-routes, and build basic platform

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express();
 
-app.get('/',  (req, res) => {
-	res.send("home page")
-})
+app.set('view-engine', 'ejs');
 
+
+
+const homeRouter = require('./routes/home');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+
+app.use('/', homeRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 app.listen(4040, () => console.log("server start in 4040"))
