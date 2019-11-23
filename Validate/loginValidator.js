f56772b19691f77afe2570cf655cd2e55c38fda6
Edgar Nikoghosyan <nikogosjan_edgar@mail.ru>
2019-11-23 14:22:24 +0400

    signup page done
    signup page done 2

--- Validate/loginValidator.js
+++ Validate/loginValidator.js
@@ -6,7 +6,7 @@ const User = require('../models/User')
 
 
 
-const REG_FIELDS = ['firstname', 'lastname', 'salary', 'phoneNumber', 'email', 'password', "resetPassword",
+const REG_FIELDS = ['firstname', 'lastname', 'salary', 'phoneNumber', 'email', 'password', "repeatPassword",
 	'birthday', 'role'
 ]
 class LoginValidator {
@@ -16,16 +16,18 @@ isRegister(req, res, next) {
 	if (size != 9 ) {
 		return Error.registerError(res)	
 	} 
-	if (!(req.body.password)){
+	if (!(req.body.password)) {
 		return Error.registerError(res, `Please add password field`);
 	}else if (req.body.password.length <= 6) {
 		return Error.registerError(res, `Too few characters for password ${req.body.password.length} it's must by 6+`)
+	} else if(req.body.password != req.body.repeatPassword){
+		return Error.registerError(res, `Passwords do not match`)
 	}
 	for(let key in req.body){
 		let fieldCheck = _.includes(REG_FIELDS, key);
 		if (!fieldCheck) {
 		return Error.registerError(res, `${key} field is not present, please fill correct`)
-		}	
+		}
 	}
 	next();
 }
