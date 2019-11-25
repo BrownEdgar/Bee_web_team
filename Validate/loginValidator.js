2f77e38627ccf8477f818c385b339b609b57d9ac
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2019-11-25 12:11:12 +0400

    user loginss

--- Validate/loginValidator.js
+++ Validate/loginValidator.js
@@ -35,8 +35,9 @@ class LoginValidator {
 
 	async isLogin(req, res, next) {
 		try {
-			const token = req.headers.authorization;
+			const token = req.headers.authorization.split(" ")[1];
 			const decoded = jwt.verify(token, process.env.SESSION_SECRET);
+		
 			req.userData = decoded;
 			next();
 		} catch (error) {
