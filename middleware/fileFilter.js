c9e5a10b62c38f7eb2cb14ce20923af031a2648e
Edgar Nikoghosyan <nikogosjan_edgar@mail.ru>
2019-11-27 20:43:40 +0400

    add 'cv' file type validation

--- middleware/fileFilter.js
+++ middleware/fileFilter.js
@@ -1,10 +1,10 @@
-
-
+const FILE_FIELDS = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf', 'application/msword']
 
 module.exports = function fileFilter(req, file, cb) {
-	if (file.mimetype !== 'application/pdf' || file.mimetype == 'application/msword') {
-		cb(new Error("cv Error, file type  must by 'png' or 'doc'"), true);
+
+	if (file.mimetype === FILE_FIELDS[0] || file.mimetype === FILE_FIELDS[1] || file.mimetype === FILE_FIELDS[2]) {
+		cb(null, true);
 	}else{
-		cb(null, false);
+		cb(new Error("cv Error: file type must by 'png', 'docx' or 'doc'."),  false);
 	}
 }
